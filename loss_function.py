1ff74732e44988217a05366d9e3d2b4c005ea63c
Jeevesh8 <creativityinczenyoga@gmail.com>
2020-08-17 22:13:50 +0530

    Some Corrections.

--- loss_function.py
+++ loss_function.py
@@ -9,7 +9,7 @@ class Tacotron2Loss(nn.Module):
     
     def __init__(self, hparams):
         super(Tacotron2Loss, self).__init__()
-        self.ce_loss = nn.CrossEntropyLoss(reduction='sum')
+        self.ce_loss = nn.CrossEntropyLoss()                                                                        #Maybe use (reduction='sum') ?
 
     def forward(self, model_output, targets, re, batched_speakers):
         mel_target, gate_target = targets[0], targets[1]
@@ -24,13 +24,13 @@ class Tacotron2Loss(nn.Module):
         gate_loss = nn.BCEWithLogitsLoss()(gate_out, gate_target)
         
         means, stddevs = re.q_zo_given_X_at_x.mean, re.q_zo_given_X_at_x.stddev
-        kl_loss = kld(re.p_zo_given_yo.distrib_lis[batched_speakers[0]], Normal(means[0], stddevs[0])).sum()
+        kl_loss = kld(Normal(means[0], stddevs[0]), re.p_zo_given_yo.distrib_lis[batched_speakers[0]]).sum()
         for i, speaker in enumerate(batched_speakers[1:], 1) :
             kl_loss += kld(Normal(means[i], stddevs[i]), re.p_zo_given_yo.distrib_lis[speaker]).sum()
         for i in range(re.p_zl_given_yl.n_disc) :
             kl_loss += ( re.q_yl_given_X[i]*kld(re.q_zl_given_X_at_x, re.p_zl_given_yl.distrib_lis[i]).sum(dim=1) ).sum()
         for i in range(re.q_yl_given_X.shape[1]) :
-            kl_loss += kld( Categorical(re.q_yl_given_X[:,i]+1e-12), re.y_l)
+            kl_loss += kld( Categorical(re.q_yl_given_X[:,i]), re.y_l)
         kl_loss = kl_loss/batched_speakers.shape[0]
         
         index_into_spkr_logits = batched_speakers.repeat_interleave(spkr_clsfir_logits.shape[1])
